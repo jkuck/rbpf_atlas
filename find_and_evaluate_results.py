b93a5aa3363310de59e6e40b4f366f5b1f3c22b3
Jonathan Kuck <jdkuck@gmail.com>
2016-12-02 19:07:03 -0800

    experiment run here:
    origRBPF_KITTI_GENDATA_learnedKFparams

--- find_and_evaluate_results.py
+++ find_and_evaluate_results.py
@@ -1,6 +1,6 @@
 import os
 
-DIRECTORY_TO_SEARCH = './results'
+DIRECTORY_TO_SEARCH = './results/origRBPF_KITTI_DATA_learnedKFparams'
 
 
 def find_and_eval_results(directory_to_search, seq_idx_to_eval=[i for i in range(21)], info_by_run=None):
